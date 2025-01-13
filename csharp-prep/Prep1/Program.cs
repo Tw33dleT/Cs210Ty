46be30d142fc14ec5510d1b49d4987d5ee83f3ec
Tw33dleT <ty_frandsen@yahoo.com>
2025-01-13 13:44:10 -0700

    added James bond

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What's your first name? ");
+        string FirstName = Console.ReadLine();
+        Console.WriteLine("and what is your last name? ");
+        string LastName = Console.ReadLine();
+        Console.WriteLine($"Ah so your name is {LastName},{FirstName} {LastName}");
     }
 }
