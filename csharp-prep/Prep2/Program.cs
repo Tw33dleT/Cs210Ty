fee56a4186657f87262ea9b7f6e46d87b8a8ea9e
Tw33dleT <ty_frandsen@yahoo.com>
2025-01-15 10:10:27 -0700

    graded the assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What's your grade?");
+        string response = Console.ReadLine();
+        int grade = int.Parse(response);
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "f";
+        }
+
+        Console.WriteLine($"your grade is {letter} ");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("you passed your class!");
+        }
+        else 
+        {
+            Console.WriteLine("welp");
+        }
     }
 }
