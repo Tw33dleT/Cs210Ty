4fa0019bbef7a6740789a086a7889159a9dc6a82
Tw33dleT <ty_frandsen@yahoo.com>
2025-01-15 19:45:57 -0700

    Added guessing game code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,10);
+        
+         int guess = -1;
+        while (guess != number)
+        {
+            Console.WriteLine("What is the magic number? 1-10 ");
+            guess = int.Parse(Console.ReadLine());
+            if (guess > number)
+            {
+             Console.WriteLine("that's too high! try again");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("that's too low! try again");
+            }
+            else if (guess == number)
+            {
+            Console.WriteLine("That's it!");
+            }
+            else 
+            {
+            Console.WriteLine("that's not a valid guess");
+            }
+        }
     }
 }
